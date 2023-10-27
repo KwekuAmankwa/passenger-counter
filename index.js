bb6af08241153cec08d17cba1fa2cc8228c4e0cc
Oak <mickyodum@gmail.com>
2023-10-27 15:16:47 +0200

    passenger counter js app

---
+++ index.js
@@ -0,0 +1,25 @@
+let count = 0
+let countEL = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+function increment(){
+    count += 1
+    countEL.textContent = count
+}
+
+function save(){
+    let previous = count + " - "
+    saveEl.textContent += previous
+    count = 0
+    countEL.textContent = count
+}
+
+// welcome message with js
+// let welcomeEL = document.getElementById("welcome-el")
+
+// let name = "Hello Kweku, "
+// let greeting = "Welcome to Scrimba!"
+
+// welcomeEL.innerText = name + greeting 
+
+// welcomeEL.innerText += "👋"
